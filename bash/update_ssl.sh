4a71b91e0ae1154a55fea6a6501ef520f0dcea13
b17Own <90437847+b17Owner@users.noreply.github.com>
2022-09-16 10:34:03 +0600

    Create update_ssl.sh

---
+++ bash/update_ssl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+VERSION='1.1.1q'
+
+cd ~
+mkdir -p src && cd src
+
+wget --no-check-certificate https://www.openssl.org/source/openssl-${VERSION}.tar.gz
+mkdir /opt/openssl
+tar xfvz openssl-${VERSION}.tar.gz --directory /opt/openssl
+export LD_LIBRARY_PATH=/opt/openssl/lib
+cd /opt/openssl/openssl-${VERSION}/
+./config --prefix=/opt/openssl --openssldir=/opt/openssl/ssl
+make
+make test
+make install
+
+touch /etc/profile.d/openssl.sh
+
+PROFILE=`cat <<_EOF_
+#!/bin/sh
+export PATH=/opt/openssl/bin:${PATH}
+export LD_LIBRARY_PATH=/opt/openssl/lib:${LD_LIBRARY_PATH}
+_EOF_
+`
+
+echo "$PROFILE" > /etc/profile.d/openssl.sh
+
+chmod +x /etc/profile.d/openssl.sh
+source /etc/profile.d/openssl.sh
